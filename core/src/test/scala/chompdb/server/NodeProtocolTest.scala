5d15dd811fd397d3cb16dcc7d60eab7430eef86d
Katherine Fellows <k@kpf.me>
2014-02-28 15:49:13 -0800

    added log statements

--- core/src/test/scala/chompdb/server/NodeProtocolTest.scala
+++ core/src/test/scala/chompdb/server/NodeProtocolTest.scala
@@ -3,6 +3,7 @@ package chompdb.server
 import chompdb._
 import chompdb.server._
 import chompdb.testing.TestUtils.createEmptyShard
+import chompdb.util.ConsoleLogger
 
 import f1lesystem.LocalFileSystem
 
@@ -20,14 +21,14 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.ShouldMatchers
 
 @org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
-class NodeProtocolTest extends WordSpec with ShouldMatchers {
+class NodeProtocolTest extends WordSpec with ShouldMatchers {  
   val testName = getClass.getSimpleName
 
   val tmpLocalRoot = LocalFileSystem.tempRoot(testName) /+ "local"
 
   val cat1 = new Catalog("Catalog1", tmpLocalRoot)
   val db1 = cat1.database("Database1")
-
+  
   db1.versionedStore.createVersion(1L)
   createEmptyShard(db1.versionedStore, 1L)
   createEmptyShard(db1.versionedStore, 1L)
@@ -38,13 +39,14 @@ class NodeProtocolTest extends WordSpec with ShouldMatchers {
   val node2 = Node("Node2")
   val allNodes = Set(node1, node2)
 
-  lazy val nodeProtocols = chomps
-    .map { case (node, chomp) => node -> new Chomp.LocalNodeProtocol(node, chomp) }
+  lazy val nodeProtocols = chomps 
+    .map { case (node, chomp) => node -> new Chomp.LocalNodeProtocol(node, chomp) } 
     .toMap
 
   val chomps: Set[(Node, Chomp)] = allNodes
     .map { node =>
       node -> new Chomp {
+        override val log = new ConsoleLogger
         override val databases = Seq(db1)
         override val localNode = node
         override val nodes = allNodes map { n => (n, Endpoint("Endpoint" + n.id takeRight 1)) } toMap
@@ -56,14 +58,14 @@ class NodeProtocolTest extends WordSpec with ShouldMatchers {
         override val executor = mock(classOf[ScheduledExecutorService])
         override val databaseUpdateFreq = 1.minute
         override val nodesAliveFreq = 1.minute
-        override val nodesContentFreq = 1.minute
-        override val servingVersionsFreq = 1.minute
+        override val nodesContentFreq = 1.minute  
+        override val servingVersionsFreq = 1.minute  
         override val rootDir = tmpLocalRoot
 
         override def nodeProtocol = nodeProtocols
 
         override def serializeMapReduce[T, U](mapReduce: MapReduce[T, U]) = "identity"
-
+              
         override def deserializeMapReduce(mapReduce: String): MapReduce[ByteBuffer, _] = mapReduce match {
           case "identity" => new MapReduce[ByteBuffer, Seq[ByteBuffer]] {
             def map(t: ByteBuffer) = Seq(t)
@@ -94,8 +96,8 @@ class NodeProtocolTest extends WordSpec with ShouldMatchers {
 
   "LocalNodeProtocol" should {
     "return the set of VersionShards available for a node, given a catalog name and database name" in {
-      chomps foreach { clientNodeAndChomp =>
-        chomps foreach { serverNodeAndChomp =>
+      chomps foreach { clientNodeAndChomp => 
+        chomps foreach { serverNodeAndChomp => 
           val nodeProtocol = clientNodeAndChomp._2.nodeProtocol(serverNodeAndChomp._1)
           nodeProtocol.availableShards(db1.catalog.name, db1.name) should be === Set((1L, 0), (1L, 1), (1L, 2))
         }
